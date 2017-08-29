0da919c75555d85249ca786b2cef1daca516fa7b
Barney Hanlon <barney@shrikeh.net>
2017-08-29 21:05:14 +0100

    Start tightening testing

--- src/Bounce.php
+++ src/Bounce.php
@@ -1,10 +1,26 @@
 <?php
 namespace Shrikeh\Bounce;
 
+use Acclimate\Container\ContainerAcclimator;
+use Pimple\Container;
+use Psr\Container\ContainerInterface;
+use Shrikeh\Bounce\ServiceProvider\Bounce as BounceServiceProvider;
+
+/**
+ * Class Bounce
+ * @package Shrikeh\Bounce
+ */
 class Bounce
 {
-    public static function container()
+    /**,
+     * @return ContainerInterface
+     */
+    public static function container(): ContainerInterface
     {
-        
+        $pimple     = new Container();
+        $pimple->register(new BounceServiceProvider());
+        $acclimator = new ContainerAcclimator();
+
+        return $acclimator->acclimate($pimple);
     }
 }
--- tests/spec/EmitterSpec.php
+++ tests/spec/EmitterSpec.php
@@ -2,14 +2,33 @@
 
 namespace spec\Shrikeh\Bounce;
 
+use EventIO\InterOp\EventInterface;
+use Shrikeh\Bounce\Dispatcher\DispatcherInterface;
 use Shrikeh\Bounce\Emitter;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Shrikeh\Bounce\Listener\ListenerAcceptorInterface;
 
 class EmitterSpec extends ObjectBehavior
 {
-    function it_is_initializable()
-    {
-        $this->shouldHaveType(Emitter::class);
+    function let(
+        ListenerAcceptorInterface $acceptor,
+        DispatcherInterface $dispatcher
+    ) {
+        $this->beConstructedThroughCreate($acceptor, $dispatcher);
+    }
+
+    function it_emits_events(
+        EventInterface $event,
+        ListenerAcceptorInterface $acceptor,
+        DispatcherInterface $dispatcher
+    ) {
+        $dispatcher->isDispatching()->willReturn(false);
+
+        $dispatcher->setDispatching()->shouldBeCalled();
+        $dispatcher->enqueue($event)->shouldBeCalled();
+        $dispatcher->dispatch($acceptor)->shouldBeCalled();
+
+        $this->emit($event);
     }
 }
